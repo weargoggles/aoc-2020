4629a7fa922c38ec393b4e2961077558c076c27f
Pete Wildsmith <pwildsmith@ebay.com>
2020-12-10 01:14:13 +0100

    golfing

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -2,19 +2,18 @@ use util::data_file;
 extern crate itertools;
 use itertools::Itertools;
 use std::io::{BufReader, BufRead};
+
 extern crate anyhow;
 
 fn main() -> anyhow::Result<()> {
-    let max_id= BufReader::new(data_file("boarding-passes.txt")?)
+    let ids: Vec<i32> = BufReader::new(data_file("boarding-passes.txt")?)
         .lines()
-        .map(|l| pass_to_id(l.unwrap().as_str()))
-        .max();
-    println!("part 1: {:#?}", max_id);
+        .map(|l| l.unwrap())
+        .map(pass_to_id)
+        .collect();
 
-    let sorted: Vec<i32> = BufReader::new(data_file("boarding-passes.txt")?)
-        .lines()
-        .map(|l| pass_to_id(l.unwrap().as_str()))
-        .sorted().collect();
+    println!("part 1: {:#?}", ids.iter().max());
+    let sorted: Vec<&i32> = ids.iter().sorted().collect();
 
     for chunk in sorted.windows(2) {
         if chunk[1] - chunk[0] > 1 {
@@ -25,17 +24,15 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn pass_to_id(pass: &str) -> i32 {
+fn pass_to_id(pass: String) -> i32 {
     let pass_val = pass.chars().fold(0, |acc, item| {
-        let acc = acc << 1;
-        let res = acc | match item {
+        acc << 1 | match item {
             'F' => 0,
             'B' => 1,
             'L' => 0,
             'R' => 1,
             _ => panic!("unexpected item in the bagging area"),
-        };
-        res
+        }
     });
     pass_val
 }
@@ -47,9 +44,9 @@ mod tests {
 
     #[test]
     fn test_pass_to_id() {
-        assert_eq!(pass_to_id("FBFBBFFRLR"), 357);
-        assert_eq!(pass_to_id("BFFFBBFRRR"), 567);
-        assert_eq!(pass_to_id("FFFBBBFRRR"), 119);
-        assert_eq!(pass_to_id("BBFFBBFRLL"), 820);
+        assert_eq!(pass_to_id("FBFBBFFRLR".to_string()), 357);
+        assert_eq!(pass_to_id("BFFFBBFRRR".to_string()), 567);
+        assert_eq!(pass_to_id("FFFBBBFRRR".to_string()), 119);
+        assert_eq!(pass_to_id("BBFFBBFRLL".to_string()), 820);
     }
 }
